6be2855340eaa8d1323026bc1db87a1d4028a4da
arten <felipe.arten@gmail.com>
2023-10-06 09:10:20 -0300

    create player tests

--- src/pages/Player.tsx
+++ src/pages/Player.tsx
@@ -18,7 +18,10 @@ export function Player() {
   }, [currentLesson])
 
   return (
-    <div className="h-screen bg-zinc-950 text-zinc-50 flex justify-center items-center">
+    <div
+      className="h-screen bg-zinc-950 text-zinc-50 flex justify-center 
+    items-center"
+    >
       <div className="flex w-[1100px] flex-col gap-6">
         <div className="flex items-center justify-between">
           <Header />
